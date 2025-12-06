6bbb020c700db547288365be95dc1d1d9cbc191d
Rullodf <defrancescolorenzo09@gmail.com>
2025-12-06 23:45:53 +0100

    ->

--- src/main/java/org/generation/MUD/Room.java
+++ src/main/java/org/generation/MUD/Room.java
@@ -15,8 +15,7 @@ public class Room {
     HashMap<String, Enemy> enemies;             //mappa dei nemici presenti nella stanza
     HashMap<String, Item> items;                //mappa degli oggetti presenti nella stanza
     HashMap<String, Room> exits;                //mappa delle Room a cui questa Room è collegata
-    OnEnter onEnter;
-    public Room(String id, String name, String welcomeText, HashMap<String, NPC> npcs, HashMap<String, Item> items, HashMap<String, Room> exits, OnEnter onEnter) {
+    public Room(String id, String name, String welcomeText, HashMap<String, NPC> npcs, HashMap<String, Item> items, HashMap<String, Room> exits) {
         RoomTypes roomTypes = new RoomTypes();
         this.id = id;
         this.welcomeText = welcomeText;
@@ -26,14 +25,10 @@ public class Room {
         this.exits = exits;
         this.exits.put("settings", roomTypes.roomTypes.get("settings"));
         this.exits.put("exit", roomTypes.roomTypes.get("main_menu"));
-        this.onEnter = onEnter;
     }
 
     void enteredRoom(Player player){
         System.out.println(welcomeText);
-        if (onEnter != null){
-            onEnter.onEnter(player);
-        }
     }
 }
 
