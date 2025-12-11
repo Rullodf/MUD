be140f6ddb9201092ee3e1f7204f5f7c13a5a4f1
Rullodf <defrancescolorenzo09@gmail.com>
2025-12-11 12:28:16 +0100

    gne

--- src/main/java/org/generation/MUD/room/Room.java
+++ src/main/java/org/generation/MUD/room/Room.java
@@ -5,6 +5,7 @@ import org.generation.MUD.item.Item;
 import org.generation.MUD.npc.Enemy;
 import org.generation.MUD.npc.NPC;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Room {
@@ -14,8 +15,8 @@ public class Room {
     HashMap<String, NPC> npcs;                  //mappa degli NPC presenti nella stanza
     HashMap<String, Enemy> enemies;             //mappa dei nemici presenti nella stanza
     HashMap<String, Item> items;                //mappa degli oggetti presenti nella stanza
-    HashMap<String, Room> exits;                //mappa delle Room a cui questa Room è collegata
-    public Room(String id, String name, String welcomeText, HashMap<String, NPC> npcs, HashMap<String, Item> items, HashMap<String, Room> exits) {
+    ArrayList<String> exits;                //mappa delle Room a cui questa Room è collegata
+    public Room(String id, String name, String welcomeText, HashMap<String, NPC> npcs, HashMap<String, Item> items, ArrayList<String> exits) {
         RoomTypes roomTypes = new RoomTypes();
         this.id = id;
         this.welcomeText = welcomeText;
@@ -23,8 +24,8 @@ public class Room {
         this.npcs = npcs;
         this.items = items;
         this.exits = exits;
-        this.exits.put("settings", roomTypes.roomTypes.get("settings"));
-        this.exits.put("exit", roomTypes.roomTypes.get("main_menu"));
+        this.exits.add("settings");
+        this.exits.add("exit");
     }
 
     void enteredRoom(Player player){
--- src/main/java/org/generation/MUD/room/RoomTypes.java
+++ src/main/java/org/generation/MUD/room/RoomTypes.java
@@ -1,5 +1,6 @@
 package org.generation.MUD.room;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 //creiamo la hashmap da usare in ogni parte del gioco
@@ -7,6 +8,22 @@ public class RoomTypes {
     HashMap<String, Room> roomTypes = new HashMap<>();
 
     public RoomTypes() {
+        HashMap<String, Room> exits = new HashMap<>();
+        roomTypes.put("start_menu", new Room(
+                "start_menu",
+                "Menu Principale",
+                "GIOCO SCHIFOMADO'",
+                null,
+                null,
+                arrayFromArgs("pianura", "settings")
+        ));
+    }
 
+    public ArrayList<String> arrayFromArgs(String... args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (String arg : args) {
+            list.add(arg);
+        }
+        return list;
     }
 }
